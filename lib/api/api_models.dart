7e6ed57ccc40a6f58f913a12a3560c20a4a81879
pawelraciborski <raciborski.p@gmail.com>
2022-10-19 09:25:45 +0200

    Adding job status related models

--- lib/api/api_models.dart
+++ lib/api/api_models.dart
@@ -28,47 +28,7 @@ class Webcam {
 
   Map<String, dynamic> toJson() => _$WebcamToJson(this);
 }
-//
-// class Printer {
-//   final State state;
-//
-//   Printer(this.state);
-// }
-//
-// class State {
-//   final String error;
-//   final Flags flags;
-//
-//   State(this.error, this.flags);
-// }
 
-// class Flags {
-//   final bool cancelling;
-//   final bool closedOrError;
-//   final bool error;
-//   final bool finishing;
-//   final bool operational;
-//   final bool paused;
-//   final bool pausing;
-//   final bool printing;
-//   final bool ready;
-//   final bool resuming;
-//   final bool sdReady;
-//
-//   Flags(
-//     this.cancelling,
-//     this.closedOrError,
-//     this.error,
-//     this.finishing,
-//     this.operational,
-//     this.paused,
-//     this.pausing,
-//     this.printing,
-//     this.ready,
-//     this.resuming,
-//     this.sdReady,
-//   );
-// }
 @JsonSerializable()
 class Command {
   final String command;
@@ -80,4 +40,89 @@ class Command {
       _$CommandFromJson(json);
 
   Map<String, dynamic> toJson() => _$CommandToJson(this);
+}
+
+@JsonSerializable()
+class File {
+  final int? date;
+  final String? display;
+  final String? name;
+  final String? origin;
+  final String? path;
+  final int? size;
+
+  File(this.date, this.display, this.name, this.origin, this.path, this.size);
+
+  factory File.fromJson(Map<String, dynamic> json) =>
+      _$FileFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FileToJson(this);
+}
+
+@JsonSerializable()
+class Progress {
+  final double? completion;
+  final int? filepos;
+  final int? printTime;
+  final int? printTimeLeft;
+  final String? printTimeLeftOrigin;
+
+  Progress(
+    this.completion,
+    this.filepos,
+    this.printTime,
+    this.printTimeLeft,
+    this.printTimeLeftOrigin,
+  );
+
+  factory Progress.fromJson(Map<String, dynamic> json) =>
+      _$ProgressFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ProgressToJson(this);
+}
+
+@JsonSerializable()
+class Job {
+  final File file;
+
+  Job(this.file);
+
+  factory Job.fromJson(Map<String, dynamic> json) =>
+      _$JobFromJson(json);
+
+  Map<String, dynamic> toJson() => _$JobToJson(this);
+}
+
+@JsonSerializable()
+class JobInfo {
+  final JobState state;
+  final Progress progress;
+
+  JobInfo(this.state, this.progress);
+
+  factory JobInfo.fromJson(Map<String, dynamic> json) =>
+      _$JobInfoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$JobInfoToJson(this);
+}
+
+enum JobState {
+  @JsonValue('Operational')
+  optional,
+  @JsonValue('Printing')
+  printing,
+  @JsonValue('Pausing')
+  pausing,
+  @JsonValue('Paused')
+  paused,
+  @JsonValue('Cancelling')
+  cancelling,
+  @JsonValue('Error')
+  error,
+  @JsonValue('Offline')
+  offline,
+  @JsonValue('Offline after error')
+  offlineAfterError,
+  @JsonValue('Opening serial connection')
+  openingSerialConnection,
 }
